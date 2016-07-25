bdd8daccefd284840ebd5be36b6102f00bb9fab4
Brandon Unbedacht <unbedachtb@gmail.com>
2016-07-25 17:21:33 -0600

    hello

--- Scripts/Controllers/weatherController.js
+++ Scripts/Controllers/weatherController.js
@@ -1,13 +1,49 @@
-angular.module("app").controller("weatherCtrl", function($scope, mainService){
-  $scope.show = false;
-  $scope.showWeather = function(place) {
-    mainService.getWeather(place)
-  .then(function(results) {
-    console.log(results);
-      $scope.data = results.main.temp;
-      $scope.cityName = results.name;
-      $scope.show = true;
-      $scope.weather = results.weather[0].description;
-    });
-  };
+angular.module("app").controller("weatherCtrl", function($scope, mainService) {
+    $scope.show = false;
+    $scope.show2 = false;
+    $scope.showWeather = function(place) {
+        mainService.getWeather(place)
+            .then(function(results) {
+                console.log(results);
+                $scope.data = results.main.temp;
+                $scope.cityName = results.name;
+                // $scope.show = true;
+                $scope.weather = results.weather[0].description;
+                if($scope.weather.includes("rain")) {
+                  console.log('rain');
+                  $(".modal-content").css({
+                    'background-image': 'url("https://images.unsplash.com/photo-1417008914239-59b898b49382?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=185476e83f71b5e28e2e8845a312d1e6")'
+                  }
+                );
+                }
+                else if ($scope.weather.includes("clouds")) {
+                  console.log('clouds');
+                  $(".modal-content").css("background-image", "url(https://images.unsplash.com/photo-1440407876336-62333a6f010f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=aef01806defa8b0c87dd1243e2db2dba)");
+                }
+                else if($scope.weather.includes("clear")) {
+                  console.log('clear');
+                  $(".modal-content").css("background-image", "url(https://images.unsplash.com/photo-1430263326118-b75aa0da770b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=74d94b3d861f5334f9ae55001c6010cc)");
+                }
+                else if($scope.weather.includes("thunder")) {
+                  console.log('clear');
+                  $(".modal-content").css("background-image", "url(https://images.unsplash.com/photo-1461511669078-d46bf351cd6e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=7d70000aca951c4030093664ace2b986)");
+                }
+                else if($scope.weather.includes("mist")) {
+                  console.log('clear');
+                  $(".modal-content").css("background-image", "url(https://images.unsplash.com/photo-1415628630193-e0372232b489?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=753bff41e34c1ef0c71a437a1c8debf9)");
+                }
+                else {
+                  console.log('else');
+                  $(".modal-content").css({
+                    "background-image": "url(https://images.unsplash.com/photo-1437239188634-1b06579eb5df?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=e101dc37c8cfe9430bf7aa1bac3c4e67)"
+                  });
+                }
+
+                $scope.show2 = true;
+                $scope.modalClose = function() {
+                    $scope.show2 = false;
+                };
+
+            });
+    };
 });
--- Scripts/Directives/helloDirective.js
+++ Scripts/Directives/helloDirective.js
@@ -1,5 +1,22 @@
 angular.module('app').directive('hello', function() {
   return {
-    templateUrl: './Views/helloDirective.html'
+    templateUrl: './Views/helloDirective.html',
+    link: function(scope, elem, attr) {
+      //if .weather contains 'rainy' then insert this backgroun image
+// var weather = $("scope.weather");
+// console.log(scope.weather);
+//       if(scope.weather.includes("rain")) {
+//         $(".modal-content").css("background", "url(https://images.unsplash.com/photo-1417008914239-59b898b49382?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=185476e83f71b5e28e2e8845a312d1e6) cover");
+//       }
+//       else if (scope.weather.includes("clouds")) {
+//         $(".modal-content").css("background", "url(https://images.unsplash.com/photo-1438045809872-34a58ff469f6?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=88bf0452a0e6e3b9e855ba4e2945048f) cover");
+//       }
+//       else if(scope.weather.includes("clear")) {
+//         $(".modal-content").css("background", "url(https://images.unsplash.com/photo-1430263326118-b75aa0da770b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=74d94b3d861f5334f9ae55001c6010cc) cover");
+//       }
+//       else {
+//         $(".modal-content").css("background", "url(https://images.unsplash.com/photo-1464375679469-e3b5b25b8943?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=87cc284420e5db6025c786ecbd1a43ce) cover");
+//       }
+    }
   };
 });
